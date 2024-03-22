93611b395bc9995044c13c0fe92cc6ee9349bea6
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-03-22 11:19:20 +0700

    refresh

---
+++ day3/siper - Copy/siper/Program.cs
@@ -0,0 +1,23 @@
+﻿using siper.ui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace siper
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ui.Jurusan_Frm());
+        }
+    }
+}
